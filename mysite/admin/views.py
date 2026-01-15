e0894abe72758ac79b921941402b6c5239d81e12
aizat20kgcom-design <aizat20kg.com@gmail.com>
2026-01-15 15:59:38 +0600

    views

---
+++ mysite/admin/views.py
@@ -0,0 +1,79 @@
+from Duolingo.mysite.database.models import UserProfile, RefreshToken, Follow, SuperFollow, FamilyFollow, MaxFollow, Language, Course, Lesson, Exercise, Option, UserProgres, XPHistory, Streak, Chat, ChatMember, Message, AddFriends, Country, Rating, LessonLevel, Achievement, UserAchievement, LessonCompletion
+from sqladmin import ModelView
+
+
+class UserProfileAdmin(ModelView, model=UserProfile):
+    column_list = [UserProfile.first_name, UserProfile.last_name]
+
+class RefreshTokenAdmin(ModelView, model=RefreshToken):
+    column_list = [RefreshToken.token]
+
+class FollowAdmin(ModelView, model=Follow):
+    column_list = [Follow.following.username]
+
+class SuperFollowAdmin(ModelView, model=SuperFollow):
+    column_list = [SuperFollow.title]
+
+class FamilyFollowAdmin(ModelView, model=FamilyFollow):
+    column_list = [FamilyFollow.title]
+
+class MaxFollowAdmin(ModelView, model=MaxFollow):
+    column_list = [MaxFollow.title]
+
+class LanguageAdmin(ModelView, model=Language):
+    column_list = [Language.name]
+
+class CourseAdmin(ModelView, model=Course):
+    column_list = [Course.title]
+
+class LessonAdmin(ModelView, model=Lesson):
+    column_list = [Lesson.title]
+
+class ExerciseAdmin(ModelView, model=Exercise):
+    column_list = [Exercise.questions]
+
+class OptionAdmin(ModelView, model=Option):
+    column_list = [Option.exercise.questions]
+
+class UserProgresAdmin(ModelView, model=UserProgres):
+    column_list = [UserProgres.completed]
+
+class XPHistoryAdmin(ModelView, model=XPHistory):
+    column_list = [XPHistory.reason]
+
+class StreakAdmin(ModelView, model=Streak):
+    column_list = [Streak.current_steak]
+
+class ChatAdmin(ModelView, model=Chat):
+    column_list = [Chat.type]
+
+class ChatMemberAdmin(ModelView, model=ChatMember):
+    column_list = [ChatMember.joined_at]
+
+class MessageAdmin(ModelView, model=Message):
+    column_list = [Message.content]
+
+class AddFriendsAdmin(ModelView, model=AddFriends):
+    column_list = [AddFriends.add_user.username]
+
+class CountryAdmin(ModelView, model=Country):
+    column_list = [Country.user_country.username]
+
+class RatingAdmin(ModelView, model=Rating):
+    column_list = [Rating.user_rating.username, Rating.streak_rating.current_streak]
+
+class LessonLevelAdmin(ModelView, model=LessonLevel):
+    column_list = [LessonLevel.level]
+
+class LessonCompletionAdmin(ModelView, model=LessonCompletion):
+    column_list = [LessonCompletion.date_completed]
+
+class AchievementAdmin(ModelView, model=Achievement):
+    column_list = [Achievement.title]
+
+class UserAchievementAdmin(ModelView, model=UserAchievement):
+    column_list = [UserAchievement.date_received]
+
+
+
+
